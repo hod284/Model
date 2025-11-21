4702dfde30edf8f272c7fe308c73232efa2a3f95
hod284 <hod284@naver.com>
2025-11-21 22:26:47 +0900

    12t

--- sever.py
+++ sever.py
@@ -145,8 +145,6 @@ def serve():
         PlateRecognizerService(),
         server
     )
-
-
     # 50051 포트에서 대기
     server.add_insecure_port("[::]:50051")
     print("PlateRecognizer gRPC server started on port 50051")
